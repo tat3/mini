e008e94c847a16769407bfcfbc0846b3681ea871
tat3 <n.tatsuki.214@gmail.com>
2018-06-09 15:28:18 +0900

    Add let binding

--- test01.ml
+++ test01.ml
@@ -1,3 +1,14 @@
+let emptyenv () = []
+
+let ext env x v = (x, v) :: env
+
+let rec lookup x env =
+    match env with
+    | [] -> failwith ("unbound variable: " ^ x)
+    | (y,v) :: tl -> if x = y then v 
+                  else lookup x tl 
+
+
 type exp =
     | IntLit of int
     | Plus of exp * exp
@@ -5,24 +16,28 @@ type exp =
     | BoolLit of bool
     | If of exp * exp * exp
     | Eq of exp * exp
+    | Var of string
+    | Let of string * exp * exp
+
 
 type value =
     | IntVal of int
     | BoolVal of bool
     
-let rec eval2 e =
-    let binop f e1 e2 =
-        match (eval2 e1, eval2 e2) with
+let rec eval3 e env =
+    let binop f e1 e2 env =
+        match (eval3 e1 env, eval3 e2 env) with
         | (IntVal (n1), IntVal (n2)) -> IntVal (f n1 n2)
         | _ -> failwith "integer values expected"
     in
     match e with
+    | Var (x) -> lookup x env
     | IntLit (n) -> IntVal (n)
-    | Plus (e1, e2) -> binop (+) e1 e2
-    | Times (e1, e2) -> binop (+) e1 e2
+    | Plus (e1, e2) -> binop (+) e1 e2 env
+    | Times (e1, e2) -> binop (+) e1 e2 env
     | Eq (e1, e2) ->
         begin
-            match (eval2 e1, eval2 e2) with
+            match (eval3 e1 env, eval3 e2 env) with
             | (IntVal (n1), IntVal (n2)) -> BoolVal (n1 = n2)
             | (BoolVal (b1), BoolVal (b2)) -> BoolVal (b1 = b2)
             | _ -> failwith "wrong value"
@@ -30,23 +45,30 @@ let rec eval2 e =
     | BoolLit (b) -> BoolVal(b)
     | If (e1, e2, e3) -> 
         begin
-            match (eval2 e1) with
-            | BoolVal(true) -> eval2 e2
-            | BoolVal(false) -> eval2 e3
+            match (eval3 e1 env) with
+            | BoolVal(true) -> eval3 e2 env
+            | BoolVal(false) -> eval3 e3 env
             | _ -> failwith "wrong value"
         end
+    | Let (x, e1, e2) ->
+        let env1 = ext env x (eval3 e1 env)
+        in eval3 e2 env1
 
-
-
+let print_value = function
+    IntVal (x) -> print_string (string_of_int x ^ "\n")
+    | BoolVal (x) -> print_string (string_of_bool x ^ "\n")
+    
 (* テスト *)
-let _ = eval2 (IntLit 1)
-let _ = eval2 (IntLit 11)
-let _ = eval2 (Plus (IntLit 1, Plus (IntLit 2, IntLit 11)))
-let _ = eval2 (Times (IntLit 1, Plus (IntLit 2, IntLit 11)))
-let _ = eval2 (If (Eq(IntLit 2, IntLit 11),
+let _ = eval3 (IntLit 1)
+let _ = eval3 (IntLit 11)
+let _ = eval3 (Plus (IntLit 1, Plus (IntLit 2, IntLit 11)))
+let _ = eval3 (Times (IntLit 1, Plus (IntLit 2, IntLit 11)))
+let _ = eval3 (If (Eq(IntLit 2, IntLit 11),
                    Times(IntLit 1, IntLit 2),
                    Times(IntLit 1, Plus(IntLit 2,IntLit 3))))
-let _ = eval2 (Eq (IntLit 1, IntLit 1))
-let _ = eval2 (Eq (IntLit 1, IntLit 2))
-let _ = eval2 (Eq (BoolLit true, BoolLit true))
-let _ = eval2 (Eq (BoolLit true, BoolLit false))    
+let _ = eval3 (Eq (IntLit 1, IntLit 1))
+let _ = eval3 (Eq (IntLit 1, IntLit 2))
+let () = print_value (eval3 (Eq (BoolLit true, BoolLit true)) [])
+let () = print_value (eval3 (Eq (BoolLit true, BoolLit false)) [])
+let () = print_value (eval3 (Let ("x", IntLit 3, Var "x")) [])
+
